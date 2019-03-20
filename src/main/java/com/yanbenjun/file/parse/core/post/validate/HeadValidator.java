0e9e0241cb037a2888b454e59324845083f17a29
yanbenjun <yanbenjunsunysk@163.com>
2019-03-20 23:13:46 +0800

    增加解析上下下文文

--- src/main/java/com/yanbenjun/file/parse/core/post/validate/HeadValidator.java
+++
@@ -1,9 +0,0 @@
-package com.yanbenjun.file.parse.core.post.validate;
-
-import com.yanbenjun.file.parse.core.ParseException;
-import com.yanbenjun.file.parse.message.HeadParseMessage;
-
-public interface HeadValidator
-{
-    public HeadParseMessage validate() throws ParseException;
-}
